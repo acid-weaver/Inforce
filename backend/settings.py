f99a83a30a02725e4e3cb1a98831dd1775b8f67e
Acid Weaver <acid.weaver@gmail.com>
2023-09-27 11:54:05 +0300

    fix secret key

--- backend/settings.py
+++ backend/settings.py
@@ -31,7 +31,7 @@ environ.Env.read_env()
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = 'django-insecure-#z*8a=jy=r=fe$rm-=2+=i6l-8s(0!6*^2c*6ew^y^bivg4yaq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
